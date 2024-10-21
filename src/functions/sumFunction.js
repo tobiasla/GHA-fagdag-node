5e1eb96e3da867a7cc1967d6b6cbd3680696f8e5
Marius Skaar Ludvigsen <marius.ludvigsen@gmail.com>
2024-10-21 21:57:51 +0200

    fix function

--- src/functions/sumFunction.js
+++ src/functions/sumFunction.js
@@ -1,13 +1,20 @@
 const { app } = require("@azure/functions");
+const { sum } = require("../kalkulator");
 
 app.http("sumFunction", {
-  methods: ["GET", "POST"],
+  methods: ["GET"],
   authLevel: "anonymous",
   handler: async (request, context) => {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get("name") || (await request.text()) || "world";
+    const queryA = request.query.get("a");
+    const queryB = request.query.get("b");
+    const a = Number(queryA);
+    const b = Number(queryB);
+    if (!queryA || !queryB || Number.isNaN(a) || Number.isNaN(b)) {
+      return { body: `Oppgi et tall for "a" og "b"` };
+    }
 
-    return { body: `Hello, ${name}!` };
+    return { body: `Summen av ${a} og ${b} er ${sum(a, b)}` };
   },
 });
